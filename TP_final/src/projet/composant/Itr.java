b9fcfafef00a23443cce3f9d962c6ba83a2b9149
geourjoa <anthony.geourjon@e.ujf-grenoble.fr>
2016-05-14 12:04:26 +0200

    Prise en compte des réponses du mail du prof.

--- TP_final/src/projet/composant/Itr.java
+++ TP_final/src/projet/composant/Itr.java
@@ -1,34 +1,39 @@
 package projet.composant;
+import java.util.Iterator;
+
 import projet.port.PortEntree;
 import projet.port.PortSortie;
 
 public class Itr extends Generateur {
+	
+	static final boolean HAUT = true;
+	
+	static final boolean FALSE = false;
 
-	private Boolean ferme;
+	private Boolean etat;
 
-	public Boolean estFerme()
+	public Boolean getEtat()
 	{
-		return ferme;
+		return etat;
 	}
 
 	public void changerEtat()
 	{
-		ferme = !ferme;
+		etat = !etat;
 	}
 
 	@Override
 	public void calculerSorties()
 	{
-		this.portsSorties[0].setValeur(this.portsEntrees[0].isValeur() && !this.estFerme());
+		changerEtat();
+		this.portsSorties[0].setValeur(etat);
 		portsSorties[0].propagerValeur();
 	}
 
 	public Itr(int id){
-		portsEntrees = new PortEntree[1];
-		portsEntrees[0] = new PortEntree(this,0);
 		portsSorties = new PortSortie[1];
 		portsSorties[0] = new PortSortie(this,0);
-		ferme = true;
+		etat = HAUT;
 		this.id = id;
 	}
 
@@ -36,15 +41,17 @@ public class Itr extends Generateur {
 	public String toString()
 	{
 
-		String s = "<" + id + "| Itr (" + portsEntrees.length + "," + portsSorties.length + "){" + ((ferme)?"ferme":"ouvert") + "}->";
+		String s = "<" + id + "| Itr (" + 0 + "," + portsSorties.length + "){" + ((etat)?"haut":"bas") + "}->";
 
-		for (int i = 0; i < portsEntrees.length; i++) 
+		for (int i = 0; i < portsSorties.length; i++) 
 		{
-			s += "#" + portsSorties[i].getId_port();
-			for (int j = 0; j < portsEntrees.length; j++) 
-			{
-				s += portsEntrees[i].getProprietairePort().getId() + "#" + portsEntrees[i].getId_port() + ",";
-			}
+			s += "#" + portsSorties[i].getId_port() +"(";
+					
+			for (Iterator<PortEntree> it = portsSorties[i].getListePortEntreeConnectes().iterator(); it.hasNext();) {
+				PortEntree pe = it.next();
+				s+=  pe.getProprietairePort().getId() + "#" + pe.getId_port() + ",";				
+			}		
+			s+=")";	
 		}
 
 		s += ">";
