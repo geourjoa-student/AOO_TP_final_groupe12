a5310785ef2945dc88bb80cd6c12b4e094e81c1e
geourjoa <anthony.geourjon@e.ujf-grenoble.fr>
2016-05-15 14:58:11 +0200

    Classe testOux terminée et fonctionnelle à pariori

--- TP_final/src/projet/test/TestOux.java
+++ TP_final/src/projet/test/TestOux.java
@@ -4,11 +4,12 @@ import projet.composant.Circuit;
 import projet.composant.Itr;
 import projet.composant.Led;
 import projet.composant.Oux;
+import projet.exception.ActionInterditeException;
 import projet.exception.CircuitNonCompletException;
 import projet.exception.ComposantInconnuException;
 import projet.exception.PortInconnuException;
 
-public class TestOux extends Circuit implements CircuitTesteur{
+public class TestOux extends Circuit implements CircuitTesteur {
 
 	public TestOux() {
 		super("TestOux");
@@ -16,12 +17,11 @@ public class TestOux extends Circuit implements CircuitTesteur{
 		ajouterComposant(new Led(2));
 		ajouterComposant(new Itr(3));
 		ajouterComposant(new Itr(4));
-		
-		
-		//Le cablage est sur, il n'y aura pas d'exception
+
+		// Le cablage est sur, il n'y aura pas d'exception
 		try {
 			cabler(3, 0, 1, 0);
-			cabler(4, 0, 1, 0);
+			cabler(4, 0, 1, 1);
 			cabler(1, 0, 2, 0);
 		} catch (ComposantInconnuException | PortInconnuException e) {
 			// TODO Auto-generated catch block
@@ -31,34 +31,50 @@ public class TestOux extends Circuit implements CircuitTesteur{
 
 	@Override
 	public void tester() {
-		//TODO A reimplement comme décrit dans l'interface
-		
+		// TODO A reimplement comme décrit dans l'interface
+
 		System.out.println("************\nTest du oux :\n************\n\n");
-		
+
 		System.out.println(this);
-		
-		
-		
+
 		try {
-			System.out.println("| Itr1 | Itr2 | Led |");
+			
+
+			// Test 0-0 -> 0
+
+			changerItr(3);
+			changerItr(4);
 			execute();
-			//TODO crée des accessuers pour les composants d'un circuit
-			//Test 0-0 -> 0
-			//Test 0-1 -> 0
-			//Test 1-0 -> 0
-			//Test 1-1 -> 0
+			System.out.println( observerItr(3) + " (+) " + observerItr(4) + " = " + observerLed(2));
 			
+			// Test 0-1 -> 0
+			changerItr(4);
+			execute();
+			System.out.println( observerItr(3) + " (+) " + observerItr(4) + " = " + observerLed(2));
+		
+			// Test 1-0 -> 0
 			
+			changerItr(3);
+			changerItr(4);
+			execute();
+			System.out.println( observerItr(3) + " (+) " + observerItr(4) + " = " + observerLed(2));
 			
-		} catch (CircuitNonCompletException e) {
+			// Test 1-1 -> 0
+			changerItr(4);
+			execute();
+			System.out.println( observerItr(3) + " (+) " + observerItr(4) + " = " + observerLed(2));
+
+		
+		
+			
+
+		} catch (CircuitNonCompletException | ComposantInconnuException | ActionInterditeException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		System.out.println();
-		
-		
-		
+
 	}
 
 }
