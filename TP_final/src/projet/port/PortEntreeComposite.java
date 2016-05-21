d89bdb3b18d9bdd3fd8baf545a2742490e3dd33b
geourjoa <anthony.geourjon@e.ujf-grenoble.fr>
2016-05-21 18:47:40 +0200

    Création d'une nouvelle sort de port d'entrée pour pallier le problème du "oux"

---
+++ TP_final/src/projet/port/PortEntreeComposite.java
@@ -0,0 +1,33 @@
+package projet.port;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import projet.composant.Composant;
+
+public class PortEntreeComposite extends PortEntree{
+	
+	private List<PortEntree> portsEntreeComposantsInternesConnectes;
+
+	public PortEntreeComposite(Composant proprietaire, int id) {
+		super(proprietaire, id);
+		
+		portsEntreeComposantsInternesConnectes = new ArrayList<PortEntree>();
+	}
+	
+	public void ajouterPortEntreeComposantInterne (PortEntree pe){
+		portsEntreeComposantsInternesConnectes.add(pe);
+	}
+	
+	public void setValeur(boolean valeur) {
+		this.valeur = valeur;
+		
+		for (Iterator<PortEntree> iterator = portsEntreeComposantsInternesConnectes.iterator(); iterator.hasNext();) {
+			PortEntree pe = (PortEntree) iterator.next();
+			
+			pe.setValeur(valeur);
+		}
+	}
+
+}
